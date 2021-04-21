97eefbce012c0e8e2a03793b3242f3f98672ba4c
Alex Kuhn <akuhn853@gmail.com>
2021-04-21 19:48:54 -0400

    creating nodes the dumb way

--- node.cpp
+++ node.cpp
@@ -11,16 +11,12 @@ class Node{
         // Node(int val){
 
         // }
-        Node(int val){ // constructor
-            if(val<value[0]){
-                value[0] = val;
-            }else if(val<value[1]){
-                value[1] = val;
-            }else if(val<value[2]){
-                value[2] = val;
-            }else{
-                value[3] = val;
-            }
+
+        Node() {}
+        Node(int val0, int val1, int val2){ // constructor
+            value[0] = val0;
+            value[1] = val1;
+            value[2] = val2;
             // if(val1==-1 && val2==-1){
             //     for(int i = 0; i<6; i++){
             //         child[i] = Node(-1);
@@ -39,7 +35,7 @@ class Node{
         }
 
         bool numChildren(){
-
+            
 
         }
 
@@ -60,32 +56,72 @@ class Node{
 // void discard(Node * removeChild);
 
 int main(){
-    fstream infile;
-    infile.open("uziTree.txt");
+    // fstream infile;
+    // infile.open("uziTree.txt");
 
-    int numLeaves = 0;
-    while(infile >> numLeaves){
-        numLeaves++;
-    }
+    int numLeaves = 15;
+    // while(!infile.eof()){
+    //     numLeaves++;
+    // }
 
-    infile.close();
+    // infile.close();
 
-    int leaves [numLeaves];
+    Node root = Node(17, 28, 39);
 
-    fstream infile2;
-    infile2.open("uziTree.txt");
-    int i = 0;
-    while(infile2 >> leaves[i]){
-        cout << leaves[i] << endl;
-    }
+    Node lvl2Node1 = Node(3,8,17);
+    *lvl2Node1.parent = root;
+    Node lvl2Node2 = Node(21, -1, 28);
+    *lvl2Node2.parent = root;
+    Node lvl2Node3 = Node(33, -1, 39);
+    *lvl2Node3.parent = root;
+    *root.child[0] = lvl2Node1;
+    *root.child[1] = lvl2Node2;
+    *root.child[2] = lvl2Node3;
 
-    infile2.close();
+    Node lvl3Node1 = Node(1,-1,3);
+    *lvl3Node1.parent = lvl2Node1;
+    Node lvl3Node2 = Node(5, -1, 8);
+    *lvl3Node2.parent = lvl2Node1;
+    Node lvl3Node3 = Node(11,14,17);
+    *lvl3Node3.parent = lvl2Node1;
+    *lvl2Node1.child[0] = lvl3Node1;
+    *lvl2Node1.child[1] = lvl3Node2;
+    *lvl2Node1.child[2] = lvl3Node3;
 
-    Node marty = Node(3);
-    cout << marty.value[3] << endl;
-    // Node canireadtheleaves[numLeaves];
+    Node lvl3Node4 = Node(19, -1, 21);
+    *lvl3Node4.parent = lvl2Node2;
+    Node lvl3Node5 = Node(24, -1, 28);
+    *lvl3Node5.parent = lvl2Node2;
+    *lvl2Node2.child[0] = lvl3Node4;
+    *lvl2Node2.child[2] = lvl3Node5;
+
+    Node lvl3Node6 = Node(31, -1, 33);
+    *lvl3Node6.parent = lvl2Node3;
+    Node lvl3Node7 = Node(36, -1, 39);
+    *lvl3Node7.parent = lvl2Node3;
+    *lvl2Node3.child[0] = lvl3Node6;
+    *lvl2Node3.child[2] = lvl3Node7;
+
+    // fstream infile2;
+    // infile2.open("uziTree.txt");
+    // int i = 0;
+    // while(i < numLeaves){
+    //     infile2 >> leaves[i];
+    //     i++;
+    // }
+
+    // infile2.close();
+    // cout << numLeaves << endl;
+
+    // for(int i = 0; i < numLeaves; i++)
+    //     cout << leaves[i] << endl;
+    //Node marty = Node(3);
+    //cout << marty.value[3] << endl;
+    // Node * leafnodes = new Node[numLeaves];
     // for(int i = 0; i<numLeaves; i++){
-    //     canireadtheleaves[i] = Node(leaves[i]); 
+    //     cout << "leaves[" << i << "]: " << leaves[i] << endl;
+    //     leafnodes[i] = Node(leaves[i]);
+    //     cout << leafnodes[i].value[0] << endl;
     // }
 
     return 0;
