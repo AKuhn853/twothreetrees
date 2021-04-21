6d74f456e05130062f9828df369d4bdb729939a9
chloetriestocomputer <cmmakdad@gmail.com>
2021-04-21 18:22:12 -0400

    meet marty

--- node.cpp
+++ node.cpp
@@ -2,21 +2,25 @@
 #include <fstream>
 using namespace std;
 
-class Node {
-        Node * parent;
-        int value[6];
-        Node * child[6];
+class Node{
     public:
+        Node * parent;
+        int value[6] = {-1,-1,-1,-1,-1,-1};
+        Node * child[6];
+    
         // Node(int val){
 
         // }
-        Node(int val0, int val1 = -1, int val2 = -1){ // constructor
-            // parent = max(val0,val1,val2);
-            value[0] = val0;
-            if(val1!=-1)
-                value[1] = val1;
-            if(val2!=-1)
-                value[2] = val2; 
+        Node(int val){ // constructor
+            if(val<value[0]){
+                value[0] = val;
+            }else if(val<value[1]){
+                value[1] = val;
+            }else if(val<value[2]){
+                value[2] = val;
+            }else{
+                value[3] = val;
+            }
             // if(val1==-1 && val2==-1){
             //     for(int i = 0; i<6; i++){
             //         child[i] = Node(-1);
@@ -77,11 +81,12 @@ int main(){
 
     infile2.close();
 
-    
-    Node canireadtheleaves[numLeaves];
-    for(int i = 0; i<numLeaves; i++){
-        canireadtheleaves[i] = Node(leaves[i]); 
-    }
+    Node marty = Node(3);
+    cout << marty.value[3] << endl;
+    // Node canireadtheleaves[numLeaves];
+    // for(int i = 0; i<numLeaves; i++){
+    //     canireadtheleaves[i] = Node(leaves[i]); 
+    // }
 
     return 0;
 }
